68a9cd8602731477e768e0c3928b2daabaab2607
vlad - name <vlad@kuznetsov.site>
2023-01-17 11:20:26 +0100

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
 from setuptools import setup
 
-setup(name='featuredeploy',
-      version='1.0.3',
-      description='Test single features',
-      url='http://github.com/resmio/resmio/featuredeploy',
-      author='Resmio Team',
-      author_email='info@resmio.com',
-      packages=['featuredeploy'],
-      scripts=['featuredeploy/featuredeploy'],
-      package_data={
-                'featuredeploy': ['deploy.sh'],
-      },
-      install_requires=[
-          'pytz>=2016.7',
-          'python-digitalocean==1.9.0',
-          'pycryptodome==3.14.1'
-      ],
-      zip_safe=False)
+if __name__ == '__main__':
+    setup()
