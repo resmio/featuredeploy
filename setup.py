3a33b8891b2cb4fc82ac46e9442c4e0d267ff506
vlad - name <vlad@kuznetsov.site>
2023-05-15 10:50:24 +0200

    update featuredeploy version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='featuredeploy',
-      version='1.0.3',
+      version='1.0.6',
       description='Test single features',
       url='http://github.com/resmio/resmio/featuredeploy',
       author='Resmio Team',
