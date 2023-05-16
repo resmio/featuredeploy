500f2f6b3d061ee2bd00cdca0f1edbb6adbbe380
vlad - name <vlad@kuznetsov.site>
2023-05-16 09:12:12 +0200

    update message

--- featuredeploy/deploy.sh
+++ featuredeploy/deploy.sh
@@ -21,7 +21,7 @@ ufw allow 80
 webfsd -p 80 -f logs.txt -b {{HTTP_AUTH_USER}}:{{HTTP_AUTH_PASS}}
 cd /root
 
-echo: "test message!"
+echo "Prepare startup script"
 
 cat > ~/startup <<- 'UniqueText550e8400e29b11d4a716446655440000'
 {{STARTUP}}
